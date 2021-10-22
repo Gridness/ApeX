7f3784642ef4e203b4a1a0e2c521864d44de9503
Gridness <61116326+Gridness@users.noreply.github.com>
2021-10-22 18:24:57 +0300

    0.1.4 update

--- ApeX.cpp
+++ ApeX.cpp
@@ -1,43 +1,50 @@
+#include <Windows.h>
+#include <string>
 #include "ApeX.h"
 
 namespace ApeX {
 
 	void Print::printMessage(std::string message, int windowWidth, char cornerStyle, char topAndBottomBorderStyle, char sideBorderStyle, bool printTop, bool printBottom) {
-        bool front = true;
-
-        if (printTop) {
-            std::cout << cornerStyle;
-            for (int i = 0; i < windowWidth; i++) {
-                std::cout << topAndBottomBorderStyle;
-            }
-            std::cout << cornerStyle << "\n";
-            std::cout << sideBorderStyle;
+        if (windowWidth < message.length()) {
+            ApeX::Error::error(ApeX::Error::errorType(1), "Width of the frame must be more or equal to the message's length", "Width of the frame "  + std::to_string(windowWidth) + " is less than the message's length " + std::to_string(message.length()));
         }
         else {
-            std::cout << sideBorderStyle;
-        }
+            bool front = true;
 
-        for (int i = message.length(); i < windowWidth; i++) {
-            if (front) {
-                message = " " + message;
+            if (printTop) {
+                std::cout << cornerStyle;
+                for (int i = 0; i < windowWidth; i++) {
+                    std::cout << topAndBottomBorderStyle;
+                }
+                std::cout << cornerStyle << "\n";
+                std::cout << sideBorderStyle;
             }
             else {
-                message = message + " ";
+                std::cout << sideBorderStyle;
             }
-            front = !front;
-        }
-        std::cout << message.c_str();
 
-        if (printBottom) {
-            std::cout << sideBorderStyle << "\n";
-            std::cout << sideBorderStyle;
-            for (int i = 0; i < windowWidth; i++) {
-                std::cout << topAndBottomBorderStyle;
+            for (int i = message.length(); i < windowWidth; i++) {
+                if (front) {
+                    message = " " + message;
+                }
+                else {
+                    message = message + " ";
+                }
+                front = !front;
             }
-            std::cout << cornerStyle << "\n";
-        }
-        else {
-            std::cout << sideBorderStyle << "\n";
+            std::cout << message.c_str();
+
+            if (printBottom) {
+                std::cout << sideBorderStyle << "\n";
+                std::cout << cornerStyle;
+                for (int i = 0; i < windowWidth; i++) {
+                    std::cout << topAndBottomBorderStyle;
+                }
+                std::cout << cornerStyle << "\n";
+            }
+            else {
+                std::cout << sideBorderStyle << "\n";
+            }
         }
 	}
 
@@ -67,18 +74,28 @@ namespace ApeX {
 
         std::cout << "\n";
 
-        ApeX::Print::printMessage("APE-X", 26, 43, 45, 124, false, false);
-        ApeX::Print::printMessage("Version 0.1 aplha", 26, 43, 45, 124, false, false);
+        ApeX::Print::printMessage("APE-X", 24, 43, 45, 124, false, false);
+        ApeX::Print::printMessage("Version 0.1.4 alpha", 24, 43, 45, 124, false, false);
+        ApeX::Print::printMessage("ALPHA. WORK IN PROGRESS", 24, 43, 45, 124, false, false);
 
         std::cout << "\n";
 
         ApeX::Copyright::displayCopyrightInfo();
+
+        Sleep(3000);
+        system("cls");
     }
 
     void Copyright::displayCopyrightInfo()
     {
         std::cout << "ApeX (c) Gridness 2021\n";
         std::cout << "This software is disributed via MIT license. All rights reserved";
+    }
+
+    void Error::error(errorType errorType, std::string errorMessage, std::string additionalInfo)
+    {
+        std::cout << "[Error: " << errorType << "] " << errorMessage << "\n";
+        std::cout <<  additionalInfo << "\n";
     }
 
 }
